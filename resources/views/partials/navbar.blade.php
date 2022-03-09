a20fc67c57c08f2858db0167fe6d12fbf19b7c76
AlghazHernanda <mohamad19015@mail.unpad.ac.id>
2022-03-09 23:42:53 +0700

    Initial commit

---
+++ resources/views/partials/navbar.blade.php
@@ -0,0 +1,57 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-danger">
+    <div class="container">
+      <a class="navbar-brand" href="/">CampEvent</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+              {{-- di bawah ini memakai operator ternary, jadi jika title = home, maka tambahkan kelas active, kalau engga kasih string kosong --}}
+            <a class="nav-link"  href="/">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="/about">About</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="/posts">Event</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="/categories">Event Categories</a>
+          </li>
+        </ul>
+
+
+        <ul class="navbar-nav ms-auto">
+          {{-- kalo udah login, tampilin ini --}}
+          @auth
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" 
+              role="button" aria-expanded="false">Welcom Back, </a>
+              {{-- {{ auth()->user()->name }} --}}
+              <ul class="dropdown-menu">
+                <li><a class="dropdown-item" href="/dashboard"> <i class="bi bi-layout-text-sidebar-reverse"></i> My Dashboard</a></li>
+                <li><hr class="dropdown-divider"></li>
+                <li>
+                  <form action="/logout" method="post" >
+                    @csrf
+                    <button type="submit" class="dropdown-item"><i class="bi bi-box-arrow-right"></i> Logout</button>
+
+                  </form>
+              </li>
+              </ul>
+            </li>
+
+          {{-- kalo belum login, tampilkan logo login --}}
+          @else 
+            <li class="nav-item">
+              <a href="/login" class="nav-link"><i class="bi bi-box-arrow-in-right"></i>
+                Login</a>
+            </li>
+          @endauth
+        </ul>
+        
+
+      </div>
+    </div>
+  </nav>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,4 @@
+@extends('layouts.main') {{-- ini memanggil file main yang di dalam layout --}}
+@section('container')
+    {{-- ini isi content --}}
+@endsection
