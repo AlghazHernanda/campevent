cd8a849dc548a350a6b45c7cc1201125e2f172f6
almer10 <hadyanalmer@gmail.com>
2022-03-28 09:51:26 +0700

    new

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,63 @@
+<footer class="jumbotron-fluid" id="Footer">
+    <div class="container">
+        <div class="row">
+            <div class="col-5">
+                <div class="about">
+                    <div class="logo">
+                        <img src="/source/img/logo.png">
+                    </div>
+                    <h3>CampEvent is well organized platform which contains
+                        event information and buying event ticket. We provide the people with the good experience in attending an event.</h3>
+                </div>
+                <div class="hr1" style="margin-top: 20px; margin-bottom: 20px;"></div>
+                <div class="about">
+                    <h3>Head Office</h3>
+                    <h3>Casa De La Madre, Jl. Kolonel Ahmad Syam No.702, Cikeruh, Jatinangor, Kabupaten Sumedang, Jawa Barat 45363</h3>
+                </div>
+            </div>
+            <div class="col-2">
+                <div class="company">
+                    <h2>Menu</h2>
+                    <ul>
+                        <li>
+                            <a class="com-link" href="">Home</a>
+                        </li>
+                        <li>
+                            <a class="com-link" href="">Event</a>
+                        </li>
+                        <li>
+                            <a class="com-link" href="">About Us</a>
+                        </li>
+                        <li>
+                            <a class="com-link" href="">FAQ</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-1">
+                <div class="vl"></div>
+            </div>
+            <div class="col-4">
+                <div class="services">
+                    <h2>Connect With Us</h2>
+                    <div>
+                        <a href=""><i class="bi bi-instagram"></i></a>
+                        <a href=""><i class="bi bi-tiktok"></i></a>
+                        <a href=""><i class="bi bi-youtube"></i></a>
+                        <a href=""><i class="bi bi-twitter"></i></a>
+                        <a href=""><i class="bi bi-telegram"></i></a>
+                        <a href=""><i class="bi bi-linkedin"></i></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+</footer>
+<!-- End Footer -->
+
+<!-- Copyright -->
+<div class="copyright" style="background-color: #7c684a; text-align: center;">
+    <div class="col-lg-12">
+        <p style="color: white;">&copy; CampEvent, 2022.</p>
+    </div>
+</div>
+<!-- End Copyright -->
