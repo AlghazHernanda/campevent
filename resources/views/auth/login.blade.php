d72af40e2204a5db2127e401fb486e38f10486a7
AlghazHernanda <mohamad19015@mail.unpad.ac.id>
2022-04-07 23:54:03 +0700

    verified email

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,11 @@
+<div class="form-group row mb-0">
+    <div class="col-md-8 offset-md-4">
+        <button type="submit" class="btn btn-primary">
+            {{ __('Login') }}
+        </button>
+
+        <a class="btn btn-link" href="{{ route('ForgetPasswordGet') }}">
+            {{ __('Forgot Your Password?') }}
+        </a>
+    </div>
+</div>
