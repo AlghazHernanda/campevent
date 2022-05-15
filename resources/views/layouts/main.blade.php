a61dcd11ef0a7c885d780e09a64c852a6c68fcc9
almer10 <hadyanalmer@gmail.com>
2022-05-15 23:46:02 +0700

    Update main.blade.php

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -2,45 +2,53 @@
 <html lang="en">
 
 <head>
-  <!-- Required meta tags -->
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-  <!-- Bootstrap CSS -->
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 
-  {{-- Boostrap Icons --}}
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css">
+    {{-- Boostrap Icons --}}
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css">
 
-  {{-- Google Font --}}
-  <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap">
+    {{-- Google Font --}}
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap">
 
-  {{-- my stylse --}}
-  <link rel="stylesheet" href="{{asset('source/css/navbar.css')}}">
-  <link rel="stylesheet" href="{{asset('source/css/faq.css')}}">
-  <link rel="stylesheet" href="{{asset('source/css/footer.css')}}">
-  <link rel="stylesheet" href="{{asset('source/css/about.css')}}">
-  <link rel="stylesheet" href="{{asset('source/css/registerevent.css')}}">
-  <link rel="stylesheet" href="{{asset('source/css/wishlist.css')}}">
+    {{-- my stylse --}}
+    <link rel="stylesheet" href="{{ asset('source/css/navbar.css') }}">
+    <link rel="stylesheet" href="{{ asset('source/css/faq.css') }}">
+    <link rel="stylesheet" href="{{ asset('source/css/footer.css') }}">
+    <link rel="stylesheet" href="{{ asset('source/css/about.css') }}">
+    <link rel="stylesheet" href="{{ asset('source/css/registerevent.css') }}">
+    <link rel="stylesheet" href="{{ asset('source/css/wishlist.css') }}">
 
-  <title>CampEvent</title>
+    <title>CampEvent</title>
 </head>
 
 <body>
 
-  {{-- manggil navbar di folder partials.navbar --}}
-  @include('partials.navbar')
+    {{-- manggil navbar di folder partials.navbar --}}
+    @include('partials.navbar')
 
-  <div class="container mt-4">
+    <div class="container mt-4">
 
-    @yield('container') {{-- jadi ini nanti isinya halaman-halaman lain, biar pake boostrap nya 1 aja --}}
+        @yield('container') {{-- jadi ini nanti isinya halaman-halaman lain, biar pake boostrap nya 1 aja --}}
 
-  </div>
+    </div>
 
-  {{-- manggil footer di folder partials.footer --}}
-  @include('partials.footer')
+    <div>
 
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+        @yield('body') {{-- jadi ini nanti isinya halaman-halaman lain, biar pake boostrap nya 1 aja --}}
+
+    </div>
+    {{-- manggil footer di folder partials.footer --}}
+    @include('partials.footer')
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous">
+    </script>
 
 
 </body>
