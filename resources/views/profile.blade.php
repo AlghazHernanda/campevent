ff234168f2b61d7ed6acf04b9525deb811d08844
AlghazHernanda <mohamad19015@mail.unpad.ac.id>
2022-05-16 13:04:37 +0700

    backend

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -33,6 +33,7 @@
                                 <div class="rounded mb-3 mb-md-0" style="padding-top: 28px;">
                                     <div class="image"> 
                                         <img class="rounded-circle img-preview img-fluid" src="bariz1.png" width="120" height="120px">
+                                        <input type="hidden" name="oldImage" value="{{ auth()->user()->image }}">
                                         <input class="form-control @error('image') is-invalid @enderror" type="file" id="image" name="image" onchange="previewImage()"> 
                                         {{--  --}}
                                     </div>
